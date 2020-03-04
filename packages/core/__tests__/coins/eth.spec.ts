4aac0d034a8de3e3a2ac7cecef9b86b8493cc11c
Youngtaek Oh <oyt311@hanmail.net>
2020-03-04 14:54:00 +0900

    feat: implement keychain, chore: change eslint path

--- packages/core/__tests__/coins/eth.spec.ts
+++ packages/core/__tests__/coins/eth.spec.ts
@@ -1,4 +1,4 @@
-import {Eth} from '../../src/coins';
+import { Eth } from '../../src/coins';
 
 describe('Eth', () => {
   let eth: Eth;
@@ -7,16 +7,16 @@ describe('Eth', () => {
   });
 
   describe('#getName()', () => {
-    it('should return eth', function () {
-      expect(eth.getName()).toEqual("eth");
+    it('should return eth', () => {
+      expect(eth.getName()).toEqual('eth');
     });
   });
 
   describe('#buildData()', () => {
-    it('should return encoded hex data', function () {
+    it('should return encoded hex data', () => {
       const data = eth.buildData({
         amount: 5,
-        to: "0x280460de5d4488DDA8e29dFb947a8D4574203E3F"
+        to: '0x280460de5d4488DDA8e29dFb947a8D4574203E3F',
       });
       expect(data).toEqual('0xe9bb84c2000000000000000000000000280460de5d4488dda8e29dfb947a8d4574203e3f0000000000000000000000000000000000000000000000000000000000000005');
     });
---
+++ packages/core/src/keychain.ts
@@ -0,0 +1,111 @@
+import * as crypto from 'crypto';
+import {
+  bytesToWord, decodeSignature, encodeSignature, toChecksum,
+} from './utils';
+
+const elliptic = require('elliptic');
+const { keccak256, keccak256s } = require('./vendor/eth-lib/hash');
+const Bytes = require('./vendor/eth-lib/bytes');
+const sjcl = require('./vendor/sjcl');
+const { fromString } = require('./vendor/eth-lib/nat');
+
+const secp256k1 = new (elliptic.ec)("secp256k1"); // eslint-disable-line
+
+const BASE_V_VALUE = 27;
+
+export interface Key {
+  address: string;
+  pub: string;
+  keyFile: string;
+}
+
+export interface KeyWithPriv extends Key {
+  priv: string;
+}
+
+export interface Keychain {
+  create(password: string): KeyWithPriv;
+
+  decryptKeyFile(keyFile: string, password: string): string;
+
+  signPayload(payload: string, keyFile: string, password: string): string;
+
+  recoverAddressFromSignature(payload: string, signature: string): string;
+}
+
+export class EthereumKeychain implements Keychain {
+  public create(password: string): KeyWithPriv {
+    const entropy = crypto.randomBytes(512 / 8);
+
+    const innerHex = keccak256(Bytes.concat(Bytes.random(32), entropy || Bytes.random(32)));
+    const middleHex = Bytes.concat(Bytes.concat(Bytes.random(32), innerHex), Bytes.random(32));
+    const privateKey = keccak256(middleHex);
+
+    const buffer = new Buffer(privateKey.slice(2), 'hex');
+    const ecKey = secp256k1.keyFromPrivate(buffer);
+    const publicKey = `0x${ecKey.getPublic(false, 'hex').slice(2)}`;
+    const publicHash = keccak256(publicKey);
+    const address = toChecksum(`0x${publicHash.slice(-40)}`);
+
+    const keyFile = this.encryptPrivToKeyFile(privateKey, password);
+    return {
+      address,
+      pub: publicKey,
+      priv: privateKey,
+      keyFile,
+    };
+  }
+
+  public decryptKeyFile(keyFile: string, password: string): string {
+    try {
+      return sjcl.decrypt(password, keyFile);
+    } catch (error) {
+      if (error.message.includes('ccm: tag doesn\'t match')) {
+        error.message = `password error - ${error.message}`;
+      }
+      throw error;
+    }
+  }
+
+  public signPayload(payload: string, keyFile: string, password: string): string {
+    const priv = this.decryptKeyFile(keyFile, password);
+    const signature = secp256k1
+      .keyFromPrivate(new Buffer(priv.slice(2), 'hex'))
+      .sign(new Buffer(payload.slice(2), 'hex'), { canonical: true });
+
+    return encodeSignature([
+      fromString(Bytes.fromNumber(BASE_V_VALUE + signature.recoveryParam)),
+      Bytes.pad(32, Bytes.fromNat(`0x${signature.r.toString(16)}`)),
+      Bytes.pad(32, Bytes.fromNat(`0x${signature.s.toString(16)}`))]);
+  }
+
+  public recoverAddressFromSignature(payload: string, signature: string) {
+    const vals = decodeSignature(signature);
+    const vrs = { v: Bytes.toNumber(vals[0]), r: vals[1].slice(2), s: vals[2].slice(2) };
+    const ecPublicKey = secp256k1.recoverPubKey(new Buffer(payload.slice(2), 'hex'), vrs, vrs.v < 2 ? vrs.v : 1 - (vrs.v % 2));
+    const publicKey = `0x${ecPublicKey.encode('hex', false).slice(2)}`;
+    const publicHash = keccak256(publicKey);
+    const address = toChecksum(`0x${publicHash.slice(-40)}`);
+    return address;
+  }
+
+  private encryptPrivToKeyFile(privateKey:string, password:string):string {
+    const randomSalt = crypto.randomBytes(8);
+    const randomIV = crypto.randomBytes(16);
+    const encryptOptions = {
+      iter: 10000,
+      ks: 256,
+      salt: [
+        bytesToWord(randomSalt.slice(0, 4)),
+        bytesToWord(randomSalt.slice(4)),
+      ],
+      iv: [
+        bytesToWord(randomIV.slice(0, 4)),
+        bytesToWord(randomIV.slice(4, 8)),
+        bytesToWord(randomIV.slice(8, 12)),
+        bytesToWord(randomIV.slice(12, 16)),
+      ],
+    };
+    return sjcl.encrypt(password, privateKey, encryptOptions);
+  }
+}
