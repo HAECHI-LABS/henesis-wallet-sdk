26c2276a401642ee994992a3a9242e209d119082
leekt216 <leekt216@gmail.com>
2020-03-10 17:03:36 +0900

    feat: implemented getMasterWallets()

--- __tests__/wallets.spec.ts
+++ __tests__/wallets.spec.ts
@@ -39,9 +39,77 @@ describe('Wallets', ()=>{
         .post('/api/v1/wallets')
         .reply(200, response);
 
-
       const masterWallet: MasterWallet = await sdk.wallets.createMasterWallet('klaytn_test_masterWallet', 'klaytn', 'password');
       expect(masterWallet.getAddress()).toEqual(response.address);
+    });
+  });
+
+  describe('#getMasterWallets()', () => {
+    jest.setTimeout(5000000);
+    it('should return array of master wallets', async () => {
+      const response = [
+        {
+          id: '20a5fa879bcb14a5c7b1e654961c3599',
+          name: 'my_wallet',
+          address: '0xzdasyoz3uz6ik4spnkzbzihiwbgoglazf3n1ihax',
+          blockchain: 'KLAYTN',
+          created_at: '2020-02-25 22:57:21',
+          updated_at: '2020-02-25 22:57:21',
+          backupKey:
+          { 
+            address: '0x716ba9752bbf769428dcd59352e4c16f64a2a856',
+            pub:
+            '0xf1c6031a12cc0c2f15ac409833ed03685420f30f30dd077aba52ee61b0e39133821e20c7f4089b55f8a4c7a650c0da41f80e6de7ef9e1f4de0eb3ebf09b360f6',
+            keyFile:
+            '{"iv":"BEySd1ljOn4Sbb6BKTqlNQ==","v":1,"iter":10000,"ks":256,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"T85A09XexMM=","ct":"qvjhYYQXObQB4ta49b6iDwLX1+aynHOM1zhdC0bIozvW9e6MmrZaRa1N+0MxjAX/fPJQYia0KJGCEgoXfSGRCF0wWhGDBM9jK9Q="}' 
+          },
+          accountKey:
+          {
+            address: '0xc6d286f3e6b43dd410bd1a79e224adb27d3a83d2',
+            pub:
+            '0x2a514115f574b9becbb7b43c4dabf2165b97cd64f492787171f54a81e8aa7b3e72cb42335a8e0a5bc774a0dc7ec2c29f74b6b239fabecce655d714dbc2f9ae03',
+            keyFile:
+            '{"iv":"gw6l4gKz7k3BjTOP23FOCA==","v":1,"iter":10000,"ks":256,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"UFHEh0shodQ=","ct":"tWPRVQIY6JGIt2uM7Pw/5nn+M4hZAIBjLfIXnxecl6VzHDzMdC0wa0jjsHUEOJCaNXWHiDcRTLl1Iwm1ql42toj9jdVUHTbL6bQ="}' 
+          }
+        },
+        {
+          id: '20a5fa879bcb14a5c7b1e654961c3599',
+          name: 'my_wallet',
+          address: '0xzdasyoz3uz6ik4spnkzbzihiwbgoglazf3n1ihax',
+          blockchain: 'KLAYTN',
+          created_at: '2020-02-25 22:57:21',
+          updated_at: '2020-02-25 22:57:21',
+          backupKey:
+          { 
+            address: '0x716ba9752bbf769428dcd59352e4c16f64a2a856',
+            pub:
+            '0xf1c6031a12cc0c2f15ac409833ed03685420f30f30dd077aba52ee61b0e39133821e20c7f4089b55f8a4c7a650c0da41f80e6de7ef9e1f4de0eb3ebf09b360f6',
+            keyFile:
+            '{"iv":"BEySd1ljOn4Sbb6BKTqlNQ==","v":1,"iter":10000,"ks":256,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"T85A09XexMM=","ct":"qvjhYYQXObQB4ta49b6iDwLX1+aynHOM1zhdC0bIozvW9e6MmrZaRa1N+0MxjAX/fPJQYia0KJGCEgoXfSGRCF0wWhGDBM9jK9Q="}' 
+          },
+          accountKey:
+          {
+            address: '0xc6d286f3e6b43dd410bd1a79e224adb27d3a83d2',
+            pub:
+            '0x2a514115f574b9becbb7b43c4dabf2165b97cd64f492787171f54a81e8aa7b3e72cb42335a8e0a5bc774a0dc7ec2c29f74b6b239fabecce655d714dbc2f9ae03',
+            keyFile:
+            '{"iv":"gw6l4gKz7k3BjTOP23FOCA==","v":1,"iter":10000,"ks":256,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"UFHEh0shodQ=","ct":"tWPRVQIY6JGIt2uM7Pw/5nn+M4hZAIBjLfIXnxecl6VzHDzMdC0wa0jjsHUEOJCaNXWHiDcRTLl1Iwm1ql42toj9jdVUHTbL6bQ="}' 
+          }
+        },
+      ];
+
+      nock(baseUrl)
+        .get('/api/v1/wallets')
+        .reply(200, response);
+
+
+      const sdk = new SDK({
+        accessToken: 'eyJhbGciOiJIUzUxMiJ9.eyJpZCI6IjQwOGMyMWQ0OGM4MGNiMDNkM2U3NWMwMTUxMTRiZTkzIiwiaXNzIjoidHhtYW5hZ2VyLWRldiIsImlhdCI6MTU4Mzc5OTY5MSwiZXhwIjoxNTg0MTU5NjkxfQ.RoGvobumJV0iBmN-0j23pgl8QlC5I02rfOVYQxL3HlYcxXRn8IUfghfQt-MvEoHG6hgIVfqzhfdLFsQqP_NgmQ',
+        secret: 'secret',
+      });
+
+      const masterWallet = await sdk.wallets.getMasterWallets();
+      expect(masterWallet.length).toEqual(2);
     });
   });
 });
