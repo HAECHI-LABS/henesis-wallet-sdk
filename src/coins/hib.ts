fea307bdee5a387f9123c159edc99dd7a336ef9b
Jun <leebduk@gmail.com>
2020-04-07 19:54:00 +0900

    chore: remove hib coin address env

--- src/coins/hib.ts
+++ src/coins/hib.ts
@@ -10,6 +10,6 @@ export class Hib extends Erc20 {
   }
 
   getAddress(): string {
-    return process.env.COIN_HIB_ADDRESS ? process.env.COIN_HIB_ADDRESS : '0xE06b40df899b9717b4E6B50711E1dc72d08184cF';
+    return '0xE06b40df899b9717b4E6B50711E1dc72d08184cF';
   }
 }
