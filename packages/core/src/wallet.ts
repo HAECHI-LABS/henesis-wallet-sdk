f1cbbba3f9a630ef2c2b6f89c0c5b841ad5d8f28
leekt216 <leekt216@gmail.com>
2020-04-10 13:48:37 +0900

    chore: changed to let

--- packages/core/src/wallet.ts
+++ packages/core/src/wallet.ts
@@ -268,8 +268,8 @@ export class MasterWallet extends EthLikeWallet {
   }
 
   hex2a(hex) {
-    var str = '';
-    for (var i = 0; i < hex.length; i += 2)
+    let str = '';
+    for (let i = 0; i < hex.length; i += 2)
       str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
     return str;
   }
