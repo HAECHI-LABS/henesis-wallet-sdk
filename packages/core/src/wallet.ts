be79e653101a1a927e178ad8a8611b88120369c5
leekt216 <leekt216@gmail.com>
2020-04-13 13:07:15 +0900

    chore: added type to hex2a function

--- packages/core/src/wallet.ts
+++ packages/core/src/wallet.ts
@@ -267,7 +267,7 @@ export class MasterWallet extends EthLikeWallet {
     await this.changePassphrase(passphrase, newPassphrase, otpCode);
   }
 
-  hex2a(hex) {
+  hex2a(hex): string {
     let str = '';
     for (let i = 0; i < hex.length; i += 2)
       str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
