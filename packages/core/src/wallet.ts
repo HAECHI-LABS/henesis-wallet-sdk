e8f528ed4be81818ff36707951f9c10963523d2a
leekt216 <leekt216@gmail.com>
2020-04-13 13:09:41 +0900

    chore: added type for parameter

--- packages/core/src/wallet.ts
+++ packages/core/src/wallet.ts
@@ -267,7 +267,7 @@ export class MasterWallet extends EthLikeWallet {
     await this.changePassphrase(passphrase, newPassphrase, otpCode);
   }
 
-  hex2a(hex): string {
+  hex2a(hex: string): string {
     let str = '';
     for (let i = 0; i < hex.length; i += 2)
       str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
