5145357a9f472302cb81020439e057229b55dc01
leekt216 <leekt216@gmail.com>
2020-04-20 12:30:40 +0900

    feat: pdf generation will be defined on client

--- packages/core/src/keychains.ts
+++ packages/core/src/keychains.ts
@@ -3,8 +3,11 @@ import {
   bytesToWord, decodeSignature, encodeSignature, toChecksum,
 } from './utils';
 import { BlockchainType } from './blockchain';
-import { KeyWithPriv } from './types';
-
+import { Key, KeyWithPriv } from './types';
+import PDFDocument from 'pdfkit';
+import QRCode from 'qrcode';
+import SVGtoPDF from 'svg-to-pdfkit';
+import { logo } from './resources/logo';
 const elliptic = require('elliptic');
 const { keccak256 } = require('./vendor/eth-lib/hash');
 const Bytes = require('./vendor/eth-lib/bytes');
@@ -14,6 +17,7 @@ const { fromString } = require('./vendor/eth-lib/nat');
 const secp256k1 = new (elliptic.ec)("secp256k1"); // eslint-disable-line
 
 const BASE_V_VALUE = 27;
+
 
 export interface Keychains {
   create(password: string): KeyWithPriv;
@@ -71,10 +75,7 @@ export class EthereumKeychains implements Keychains {
     } catch (error) {
       if (error.message.includes('ccm: tag doesn\'t match')) {
         error.message = `password error - ${error.message}`;
-      } else if (error.message === 'sjcl.exception.corrupt is not a constructor') {
-        error.message = 'password error';
       }
-
       throw error;
     }
   }
@@ -146,3 +147,112 @@ export class EthereumKeychains implements Keychains {
     return keys.length > 0 ? keys[0] : null;
   }
 }
+
+export class RecoveryKit {
+  private name: string;
+  private blockchain: BlockchainType;
+  private henesisKey: Key;
+  private accountKey: KeyWithPriv;
+  private backupKey: KeyWithPriv;
+  private encryptedPassphrase: string
+
+  public constructor(
+    name: string,
+    blockchain: BlockchainType,
+    henesisKey: Key,
+    accountKey: KeyWithPriv,
+    backupKey: KeyWithPriv,
+    encryptedPassphrase: string
+  ){
+    this.name = name;
+    this.blockchain = blockchain;
+    this.henesisKey = henesisKey;
+    this.accountKey = accountKey;
+    this.backupKey = backupKey;
+    this.encryptedPassphrase = encryptedPassphrase;
+  }
+
+  async generatePdf(): Promise<PDFDocument> {
+    const docs = new PDFDocument({ size: 'A4' });
+    docs
+    .font(`Helvetica`)
+    .fontSize(24)
+    .fillColor('#060607')
+    .text(this.name, 36, 36);
+    // write wallet description
+    docs
+    .font(`Helvetica`)
+    .fontSize(10)
+    .fillColor('#3A4044')
+    .text(`Platform : ${this.camelize(this.blockchain)}`, 36, 76)
+    .text(`Created Time : ${this.getFormattedDate(new Date())}`, 36, 91);
+    // write note
+    docs
+    .font(`Helvetica`)
+    .fontSize(9)
+    .fillColor('#F5405B')
+    .text('This is a Recovery Kit to recover your keys when you lost them. Print this document, or keep it securely offline.', 36, 134, { width: 305 });
+    // draw logo
+    SVGtoPDF(docs, logo, 468, 36, { width: 91, preserveAspectRatio: 'xMinYMin' });
+    await this.setQRCode(docs, 'A. Account Key', 'This is your private key, encrypted with your passphrase.', this.accountKey.keyFile, 36, 224);
+    await this.setQRCode(docs, 'B. Backup Key', 'This is your backup private key, encrypted with your passphrase.', this.backupKey.keyFile, 36, 382);
+    await this.setQRCode(docs, 'C. Henesis Key', 'This is the public part of the key that Henesis will use to co-sign transactions with you on your wallet.', this.henesisKey.address, 36, 540);
+    await this.setQRCode(docs, 'D. Encrypted Wallet Passphrase', 'This is the wallet password, encrypted client-side with a key held by Henesis.', this.encryptedPassphrase, 36, 681);
+    docs.end();
+    return docs;
+  }
+
+  async setQRCode(docs : PDFDocument, name: string, desc: string, data : string, x : number, y : number) : Promise<void> {
+    const qr = await QRCode.toString(data, { type: 'svg', color: { light: '0000' }, margin: 0 });
+    docs
+    .font(`Helvetica`)
+    .fontSize(13)
+    .fillColor('#060607')
+    .text(name, x, y)
+    .fontSize(9)
+    .fillColor('#748089')
+    .text(desc, x, docs.y + 3, { width: 340 })
+    .fillColor('#465365')
+    .text('Data : ', x, docs.y + 12)
+    .fillColor('#465365')
+    .text(data, x, docs.y + 5, { width: 390, align: 'justify' });
+    SVGtoPDF(docs, qr, 453, y, { width: 105, height: 105, preserveAspectRatio: 'xMinYMin' });
+  }
+
+  getFormattedDate(date : Date) : string {
+    const yyyy = date.getFullYear().toString();
+    const mm = (date.getMonth() + 1).toString();
+    const dd = date.getDate().toString();
+    return `${yyyy}/${mm[1] ? mm : `0${mm[0]}`}/${dd[1] ? dd : `0${dd[0]}`}`;
+  }
+
+  camelize(data : string) : string {
+    const str = data.toLowerCase();
+    return str.substr(0, 1).toUpperCase() + str.substr(1);
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  getBlockchain(): BlockchainType {
+    return this.blockchain;
+  }
+
+  getHenesisKey(): Key {
+    return this.henesisKey;
+  }
+  
+  getAccountKey(): KeyWithPriv {
+    return this.accountKey;
+  }
+
+  getBackupKey(): KeyWithPriv{
+    return this.backupKey;
+  }
+
+  getEncryptedPassphrase(): string {
+    return this.encryptedPassphrase;
+  }
+}
+
