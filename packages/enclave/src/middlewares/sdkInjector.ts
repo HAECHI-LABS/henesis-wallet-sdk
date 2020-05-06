d82c1908f583c8d0e0977a6803264bb39d8e6727
AgwaB <leesd556@gmail.com>
2020-05-06 17:06:01 +0900

    feat: set gas price as input parameter

--- packages/enclave/src/middlewares/sdkInjector.ts
+++ packages/enclave/src/middlewares/sdkInjector.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { SDK } from '@haechi-labs/henesis-wallet-core';
-import { Env } from "@haechi-labs/henesis-wallet-core/lib/sdk";
+import { Env } from '@haechi-labs/henesis-wallet-core/lib/sdk';
 
 export default (req: express.Request, resp: express.Response, next: express.NextFunction) => {
   let accessToken; let secret;
@@ -16,16 +16,16 @@ export default (req: express.Request, resp: express.Response, next: express.Next
   }
 
   let env: Env = Env.Prod;
-  if (process.env.NODE_ENV === "development"){
-    env = Env.Dev
+  if (process.env.NODE_ENV === 'development') {
+    env = Env.Dev;
   }
-  if (process.env.NODE_ENV === "test"){
-    env = Env.Test
+  if (process.env.NODE_ENV === 'test') {
+    env = Env.Test;
   }
   req.sdk = new SDK({
     accessToken,
     secret,
-    env: env
+    env,
   });
   next();
 };
