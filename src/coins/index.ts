9b91fdf9a31a089d86e0da136cd2e761ba6bc8c9
AgwaB <leesd556@gmail.com>
2020-05-07 17:42:14 +0900

    chore: add evt coin

--- src/coins/index.ts
+++ src/coins/index.ts
@@ -3,11 +3,13 @@ export { Klay } from './klay';
 export { Hib } from './hib';
 export { Hcut } from './hcut';
 export { Drm } from './drm';
+export { EthEvt, KlayEvt } from './evt';
 
 export enum Ticker {
   Eth = 'Eth',
   Klay = 'Klay',
   Hib = 'Hib',
   Hcut = 'Hcut',
-  Drm = 'Drm'
+  Drm = 'Drm',
+  Evt = 'Evt'
 }
