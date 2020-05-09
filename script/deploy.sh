aca6e8e526a301dc339f5ec2c8e3645e7757d4c3
AgwaB <leesd556@gmail.com>
2020-05-09 18:45:42 +0900

    feat: add travis

---
+++ script/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
+
+echo $1
+
+if [[ $1 = "prod" ]]; then
+  TAG="$TRAVIS_TAG"
+fi
+
+docker build -t haechi/sdk-enclave:latest .
+docker push haechi/sdk-enclave:latest
+
+docker tag haechi/sdk-enclave:latest haechi/sdk-enclave:${TAG}
+docker push haechi/sdk-enclave:${TAG}
