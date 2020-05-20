7b713dc9d0b8696853633dd5ad2c2ed5b8fcb10e
KiHyun Kim <kkh555999@naver.com>
2020-05-20 14:33:08 +0900

    style: delete unused import

--- packages/enclave/src/controllers/wallet.controller.ts
+++ packages/enclave/src/controllers/wallet.controller.ts
@@ -1,4 +1,4 @@
-import express, { request } from 'express';
+import express from 'express';
 import {
   MasterWalletData, Transaction, UserWallet, UserWalletData,
 } from '@haechi-labs/henesis-wallet-core/lib/wallet';
