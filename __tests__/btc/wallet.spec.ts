9d1141529307015f749e9da8fd1c89dc9f5db532
Jun <leebduk@gmail.com>
2020-05-28 17:30:28 +0900

    feat: implement get balance, transactions

--- __tests__/btc/wallet.spec.ts
+++ __tests__/btc/wallet.spec.ts
@@ -1,20 +1,20 @@
 import { BTCWallets } from '../../src/btc/wallets';
 import { HttpClient } from '../../src/httpClient';
-import { DefaultBTCKeyChains } from '../../src/btc/keychains';
 import BN from 'bn.js';
 import { Transaction } from '../../src/btc/wallet';
+import { DefaultBtcKeyChains } from "../../src/btc/keychains";
 
 describe('BTCMasterWallet', () => {
   jest.setTimeout(50000);
   describe('#transfer()', () => {
-    it('should create master wallet', async () => {
+    it('should transfer btc', async () => {
       const wallets = new BTCWallets(
         new HttpClient({
           accessToken: 'accessToken',
           secret: 'secret',
           url: 'http://localhost:8080/api/v2',
         }) as any,
-        new DefaultBTCKeyChains(),
+        new DefaultBtcKeyChains(),
       );
       const wallet = await wallets.getWallet(
         'a3c2f4128427658ec4dcb668ec799c65',
@@ -26,6 +26,43 @@ describe('BTCMasterWallet', () => {
       );
 
       console.log(tx);
+    });
+  });
+  describe('#getBalance()', () => {
+    it('should get balance of wallet', async () => {
+      const wallets = new BTCWallets(
+        new HttpClient({
+          accessToken: 'accessToken',
+          secret: 'secret',
+          url: 'http://localhost:8080/api/v2',
+        }) as any,
+        new DefaultBtcKeyChains(),
+      );
+      const wallet = await wallets.getWallet(
+        'a3c2f4128427658ec4dcb668ec799c65',
+      );
+      const { balance } = await wallet.getBalance();
+      console.log(balance);
+    });
+  });
+
+  describe('#getTransactions()', () => {
+    it('should get transactions of wallet', async () => {
+      const wallets = new BTCWallets(
+        new HttpClient({
+          accessToken: 'accessToken',
+          secret: 'secret',
+          url: 'http://localhost:8080/api/v2',
+        }) as any,
+        new DefaultBtcKeyChains(),
+      );
+      const wallet = await wallets.getWallet(
+        'a3c2f4128427658ec4dcb668ec799c65',
+      );
+      const txs = await wallet.getTransactions();
+      txs.results.forEach(tx =>{
+        console.log(tx);
+      });
     });
   });
 });
