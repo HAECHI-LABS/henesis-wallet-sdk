84bb807236f9e16a32c9c4e6103e0babf9e98991
BoBeenLee <globaldev@naver.com>
2020-06-08 16:02:07 +0900

    refac: add prefix eth, btc events

--- packages/core/src/btc/module.ts
+++ packages/core/src/btc/module.ts
@@ -2,18 +2,21 @@ import { Client } from '../httpClient';
 import { ModuleOptions } from '../module';
 import { BtcWallets } from "./wallets";
 import { BtcKeychains, DefaultBtcKeyChains } from "./keychains";
+import { BtcEvents } from "./events";
 
 export class BtcModule {
-  private readonly client: Client;
-
   private readonly wallets: BtcWallets;
 
+  public readonly events: BtcEvents;
+  
   private readonly keychains: BtcKeychains;
+  
+  private readonly client: Client;
 
   constructor(options: ModuleOptions) {
     this.client = options.client;
-
     this.keychains = new DefaultBtcKeyChains();
     this.wallets = new BtcWallets(this.client, this.keychains);
+    this.events = new BtcEvents(this.client);
   }
 }
