25e603fe286a7e1b0c6ba22e746fbc09e2481c63
BoBeenLee <globaldev@naver.com>
2020-06-08 16:04:08 +0900

    refac: extract events

---
+++ packages/core/src/events.ts
@@ -0,0 +1,37 @@
+import * as BN from 'bn.js';
+import { PaginationOptions } from "./types";
+import { BlockchainType } from "./blockchain";
+
+export interface Event {
+    createdAt: string;
+    status: EventStatusType;
+    toAddress: string;
+    transactionHash: string;
+    walletId: string;
+  }
+  
+  export enum EventStatusType {
+    PENDING = 'PENDING',
+    FAILED = 'FAILED',
+    MINED = 'MINED',
+    CONFIRMED = 'CONFIRMED',
+  }
+  
+  export enum TransferType {
+    WITHDRAWAL = 'WITHDRAWAL',
+    DEPOSIT = 'DEPOSIT',
+  }
+  
+  export interface ValueTransferEvent extends Event {
+    amount: BN;
+    coinSymbol: string;
+    from: string;
+    to: string;
+    transferType: TransferType;
+  }
+  
+  export interface EventPaginationOptions extends PaginationOptions {
+    transactionHash?: string;
+    status?: EventStatusType;
+    blockchain?: BlockchainType;
+  }
