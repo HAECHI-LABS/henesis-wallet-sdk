9f63bbf893caf06341d3d810ba5ea9e42ae86a7f
BoBeenLee <globaldev@naver.com>
2020-06-09 18:57:43 +0900

    fix: add string test

--- packages/core/__tests__/utils/string.ts
+++ packages/core/__tests__/utils/string.ts
@@ -5,10 +5,14 @@ describe('string', () => {
   describe('#toSnakeCase', () => {
     it('should make toSnakeCase', () => {
       expect(toSnakeCase('toSnakeCase')).toEqual('to_snake_case');
+      expect(toSnakeCase('to_snake_case')).toEqual('to_snake_case');
+      expect(toSnakeCase('tosnakecase')).toEqual('tocamelcase');
     });
 
     it('should make toCamelCase', () => {
         expect(toSnakeCase('to_camel_case')).toEqual('toCamelCase');
+        expect(toSnakeCase('toCamelCase')).toEqual('toCamelCase');
+        expect(toSnakeCase('tocamelcase')).toEqual('tocamelcase');
       });
   });
 });
