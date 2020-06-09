0b192805f0172f770cd7c6b1c6a7f37241615563
BoBeenLee <globaldev@naver.com>
2020-06-09 11:49:34 +0900

    feat: add typings d.ts

---
+++ packages/core/src/typings/index.d.ts
@@ -0,0 +1,16 @@
+declare module 'moment/min/moment-with-locales';
+
+type Subtract<T, V> = Pick<T, Exclude<keyof T, keyof V>>;
+type RetrieveAsyncFunc<T extends (...args: any[]) => any> = ReturnType<
+  T
+> extends Promise<infer U>
+  ? U
+  : never;
+type RemoveKeys<T, K extends Array<keyof T>> = Pick<
+  T,
+  Exclude<keyof T, K[keyof K]>
+>;
+type RequireProperty<T, P extends keyof T> = T & { [K in P]-?: T[P] };
+type NoUndefinedField<T> = {
+  [P in keyof T]-?: NoUndefinedField<NonNullable<T[P]>>;
+};
