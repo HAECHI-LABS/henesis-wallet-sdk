2be4f29dadaf88288e3579850e8e04d306686a99
Jun <leebduk@gmail.com>
2020-06-19 18:56:54 +0900

    test: rename utils test files and fix error

--- packages/core/__tests__/utils/string.spec.ts
+++ packages/core/__tests__/utils/string.spec.ts
@@ -1,18 +1,18 @@
 import BN from 'bn.js';
 import { toSnakeCase, toCamelCase } from '../../src/utils/string';
 
-describe('string', () => {
+describe('string.spec.ts', () => {
   describe('#toSnakeCase', () => {
     it('should make toSnakeCase', () => {
       expect(toSnakeCase('toSnakeCase')).toEqual('to_snake_case');
       expect(toSnakeCase('to_snake_case')).toEqual('to_snake_case');
-      expect(toSnakeCase('tosnakecase')).toEqual('tocamelcase');
+      expect(toSnakeCase('tosnakecase')).toEqual('tosnakecase');
     });
 
     it('should make toCamelCase', () => {
-      expect(toSnakeCase('to_camel_case')).toEqual('toCamelCase');
-      expect(toSnakeCase('toCamelCase')).toEqual('toCamelCase');
-      expect(toSnakeCase('tocamelcase')).toEqual('tocamelcase');
+      expect(toCamelCase('to_camel_case')).toEqual('toCamelCase');
+      expect(toCamelCase('toCamelCase')).toEqual('toCamelCase');
+      expect(toCamelCase('tocamelcase')).toEqual('tocamelcase');
     });
   });
 });
--- packages/core/__tests__/utils/url.spec.ts
+++ packages/core/__tests__/utils/url.spec.ts
@@ -1,7 +1,7 @@
 import BN from 'bn.js';
 import { makeQueryString } from '../../src/utils/url';
 
-describe('url', () => {
+describe('url.spec.ts', () => {
   describe('#makeQueryString', () => {
     it('should make makeQueryString', () => {
       expect(makeQueryString({ a: 1, b: 2 })).toEqual('a=1&b=2');
