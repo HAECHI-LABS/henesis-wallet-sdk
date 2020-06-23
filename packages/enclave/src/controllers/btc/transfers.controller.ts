61412025709e1dfe2a666462613d0470368b956e
Jun <leebduk@gmail.com>
2020-06-24 00:00:39 +0900

    feat: implement btc enclave api

---
+++ packages/enclave/src/controllers/btc/transfers.controller.ts
@@ -0,0 +1,31 @@
+import AbstractController from "../controller";
+import { Controller } from "../../types";
+import express, { request } from "express";
+import { Pagination } from "@haechi-labs/henesis-wallet-core/lib/types";
+import { Transfer } from "@haechi-labs/henesis-wallet-core/lib/btc/transfers";
+
+export default class TransfersController extends AbstractController implements Controller {
+  private path = "/api/v2/btc/transfers";
+
+  constructor() {
+    super();
+    this.initRoutes();
+  }
+
+  protected initRoutes() {
+    this.router.get(`${this.path}`, this.promiseWrapper(this.getTransfers));
+  }
+
+  private async getTransfers(
+    req: express.Request
+  ): Promise<Pagination<Transfer>> {
+    return await req.sdk.btc.transfers.getTransfers({
+      page: +req.query.page,
+      size: +req.query.size,
+      sort: req.query.sort as string,
+      walletId: req.query.walletId as string,
+      transactionHash: req.query.transactionHash as string,
+      address: req.query.address as string,
+    });
+  }
+}
