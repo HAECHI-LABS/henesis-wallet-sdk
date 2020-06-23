206dfed2aa5a5346e1e9c871987c26a0adff13cc
Jun <leebduk@gmail.com>
2020-06-23 19:56:44 +0900

    fix: fix verify address bug and add test cases

--- packages/core/__tests__/btc/wallets.spec.ts
+++ packages/core/__tests__/btc/wallets.spec.ts
@@ -3,8 +3,8 @@ import { HttpClient } from '../../src/httpClient';
 import { BtcKeyChains } from '../../src/btc/keychains';
 import { Env } from "../../src";
 
-describe.skip('BtcWallets', () => {
-  describe('#createMasterWallet()', () => {
+describe('BtcWallets', () => {
+  describe.skip('#createMasterWallet()', () => {
     it('should create master wallet', async () => {
       const wallets = new BtcWallets(
         Env.Local,
@@ -22,6 +22,50 @@ describe.skip('BtcWallets', () => {
       );
 
       console.log(masterWallet.getData());
+    });
+  });
+
+  describe('#verifyAddress()', () => {
+    it('should return true when address is valid', async () => {
+      const wallets = new BtcWallets(
+        Env.Local,
+        new HttpClient({
+          accessToken: 'accessToken',
+          secret: 'secret',
+          url: 'http://localhost:8080/api/v2/btc',
+        }) as any,
+        new BtcKeyChains(),
+      );
+
+      expect(wallets.verifyAddress('2My2DEuwrF9XGb9vCHN9XQaNYgBya4kQNJo')).toEqual(true);
+    });
+
+    it('should return false when address is invalid', async () => {
+      const wallets = new BtcWallets(
+        Env.Local,
+        new HttpClient({
+          accessToken: 'accessToken',
+          secret: 'secret',
+          url: 'http://localhost:8080/api/v2/btc',
+        }) as any,
+        new BtcKeyChains(),
+      );
+
+      expect(wallets.verifyAddress('invalid')).toEqual(false);
+    });
+
+    it('should return false when address is not from mainnet', async () => {
+      const wallets = new BtcWallets(
+        Env.Prod,
+        new HttpClient({
+          accessToken: 'accessToken',
+          secret: 'secret',
+          url: 'http://localhost:8080/api/v2/btc',
+        }) as any,
+        new BtcKeyChains(),
+      );
+
+      expect(wallets.verifyAddress('2My2DEuwrF9XGb9vCHN9XQaNYgBya4kQNJo')).toEqual(false);
     });
   });
 });
