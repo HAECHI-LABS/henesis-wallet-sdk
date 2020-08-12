98e42bb3f5aa8c031c592d46f0395b04d3f441f5
JhChoy <jchoyeclipse@gmail.com>
2020-08-13 01:04:20 +0900

    fix: add example, fix interfaces and delete public

---
+++ example/allowed-ip.ts
@@ -0,0 +1,28 @@
+/// <reference path="../src/typings/index.d.ts" />
+import { SDK } from "../src";
+import "dotenv/config";
+
+async function main() {
+  const sdk = new SDK({
+    accessToken: process.env.ACCESS_TOKEN,
+    secret: process.env.SECRET,
+    url: process.env.URL,
+  });
+  console.log(await sdk.organizations.activateAllowedIp("otp"));
+  console.log(await sdk.organizations.inactivateAllowedIp("otp"));
+  console.log(await sdk.organizations.getAllowedIP("id"));
+  console.log(await sdk.organizations.getAllowedIPs());
+  console.log(await sdk.organizations.addAllowedIP({
+    ipAddress: "ipAddress",
+    label: "label",
+    otpCode: "otp",
+  }));
+  console.log(await sdk.organizations.patchAllowedIpLabel({
+    id: "id",
+    label: "label",
+    otpCode: "otp",
+  }));
+  console.log(await sdk.organizations.deleteAllowedIp("id", "otp"));
+}
+
+main().catch((e) => console.error(e));
