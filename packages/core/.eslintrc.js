d66311252d9a2c9808c6add7718be35d91401f9e
JhChoy <jchoyeclipse@gmail.com>
2020-08-13 12:15:19 +0900

    fix: fix options bug and endpoints, make pass body with delete method

--- packages/core/.eslintrc.js
+++ packages/core/.eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   rules: {
     'require-jsdoc': 'off',
-    'no-unused-vars': 'off',
+    'no-unused-vars': 'off', // warn으로 설정할 경우, 사용하지 않은 import들을 정상적으로 잡지 못함
     'new-cap': 'warn',
     'prefer-rest-params': 'warn',
   }
