e7e1688282d00f1e5c80f9a938a5e46bf7f1c319
JhChoy <jchoyeclipse@gmail.com>
2020-08-19 15:42:35 +0900

    fix: add coinId to Balance

--- packages/core/src/eth/henesisKeys.ts
+++ packages/core/src/eth/henesisKeys.ts
@@ -1,7 +1,7 @@
 import { Client } from "../httpClient";
 import { Balance, Key } from "../types";
 import { KeyDTO, BalanceDTO } from "../__generate__/eth";
-import { BNConverter } from "../utils/common";
+import { BNConverter } from "../";
 
 export class HenesisKeys {
   private readonly client: Client;
@@ -12,19 +12,19 @@ export class HenesisKeys {
     this.client = client;
   }
 
-  public async getHenesisKey(): Promise<Key> {
-    const response = await this.client.get<RequireProperty<KeyDTO, "pub">>(
+  getHenesisKey(): Promise<Key> {
+    return this.client.get<RequireProperty<KeyDTO, "pub">>(
       `${this.baseUrl}/me`
     );
-    return response;
   }
 
-  public async getHenesisKeyBalance(): Promise<Balance> {
+  async getHenesisKeyBalance(): Promise<Balance> {
     const response = await this.client.get<BalanceDTO>(
       `${this.baseUrl}/balance`
     );
-    const { coinType, amount, name, symbol } = response;
+    const { coinId, coinType, amount, name, symbol } = response;
     return {
+      coinId: coinId,
       coinType: coinType as any,
       amount: BNConverter.hexStringToBN(String(amount)),
       name,
