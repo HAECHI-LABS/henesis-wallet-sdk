afa2d60997ddfca7353c78903afe8f49f8b8eb0d
BoBeenLee <globaldev@naver.com>
2020-09-02 13:59:47 +0900

    feat: apply swagger api axios depositaddress

---
+++ packages/core/example/get-deposit-address.ts
@@ -0,0 +1,22 @@
+/// <reference path="../src/typings/index.d.ts" />
+import { SDK } from "../src";
+import { BtcMasterWallet } from "../src/btc/wallet";
+import "dotenv/config";
+
+async function main() {
+  const sdk = new SDK({
+    accessToken: process.env.ACCESS_TOKEN,
+    secret: process.env.SECRET,
+    url: process.env.URL,
+  });
+
+  const wallet: BtcMasterWallet = await sdk.btc.wallets.getWallet(
+    "a827891a90d51512dfed242235394c0f"
+  );
+  const depositAddress = await wallet.getDepositAddress(
+    "ea145b9983e007177727944c04344db1"
+  );
+  console.log(depositAddress);
+}
+
+main().catch((e) => console.error(e));
