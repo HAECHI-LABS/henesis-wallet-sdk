7b5e38e6272a7d72a0818769c99b49a7f0493342
LarryKwon <32338616+lawliet0521LarryK@users.noreply.github.com>
2020-11-09 17:53:20 +0900

    feat: get enclave version (#315)

---
+++ packages/enclave/src/controllers/version/version.controller.ts
@@ -0,0 +1,26 @@
+import AbstractController from "./../controller";
+import { Controller } from "../../types";
+
+export interface EnclaveVersionDTO {
+    version: string;
+}
+
+export default class VersionController
+    extends AbstractController
+    implements Controller {
+    constructor() {
+        super();
+        this.initRoutes();
+    }
+
+    public initRoutes() {
+        this.router.get(`/version`, this.promiseWrapper(this.getEnclaveVersion));
+    }
+
+    private async getEnclaveVersion(): Promise<EnclaveVersionDTO> {
+        const packageJson = require("root-require")("./package.json");
+        return {
+            version: packageJson.version,
+        };
+    }
+}
