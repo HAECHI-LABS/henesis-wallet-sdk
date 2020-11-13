56d339619beb6a16817f9e21b2e22249d2199955
Jun <leebduk@gmail.com>
2020-11-13 12:34:31 +0900

    feat: add version controller

--- packages/enclave/src/index.ts
+++ packages/enclave/src/index.ts
@@ -7,6 +7,7 @@ import CoinController from './controllers/coin.controller';
 import EventController from './controllers/event.controller';
 import GasUsagesController from './controllers/gasusages.controller';
 import TransactionController from './controllers/transactions.controller';
+import VersionController from "./controllers/version.controller";
 
 function main() {
   try {
@@ -18,6 +19,7 @@ function main() {
         new GasUsagesController(),
         new TransactionController(),
         new WalletController(),
+        new VersionController()
       ],
       port: 3000,
       hostname: '0.0.0.0',
