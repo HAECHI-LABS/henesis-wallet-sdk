67012778ea42843b9a383eaafb2c9f3f9a93ddca
JhChoy <jchoyeclipse@gmail.com>
2020-11-19 15:16:23 +0900

    refac: refac building transfer payload at coin and resolving coin

--- __tests__/coins/eth.spec.ts
+++ __tests__/coins/eth.spec.ts
@@ -1,9 +1,7 @@
 import BN from "bn.js";
 import { Eth } from "../../src/eth";
 import { CoinDTO, Blockchain } from "../../src/__generate__/eth";
-import {
-  WalletWithdrawalPolicyDTO,
-} from "../../src/__generate__/btc";
+import { EthLikeWallet } from "../../src/eth/wallet";
 
 describe("Eth", () => {
   let eth: Eth;
@@ -27,13 +25,18 @@ describe("Eth", () => {
     });
   });
 
-  describe("#buildData()", () => {
-    it("should return encoded hex data", () => {
-      const data = eth.buildTransferData(
+  describe("#buildTransferMultiSigPayload()", () => {
+    it("should has encoded hex data", async () => {
+      const mockWallet = {} as EthLikeWallet;
+      mockWallet.getAddress = jest.fn();
+      mockWallet.getNonce = jest.fn();
+
+      const data = await eth.buildTransferMultiSigPayload(
+        mockWallet,
         "0x280460de5d4488DDA8e29dFb947a8D4574203E3F",
         new BN(5)
       );
-      expect(data).toEqual(
+      expect(data.hexData).toEqual(
         "0xe9bb84c2000000000000000000000000280460de5d4488dda8e29dfb947a8d4574203e3f0000000000000000000000000000000000000000000000000000000000000005"
       );
     });
