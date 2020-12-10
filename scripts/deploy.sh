b9f2e255c73c58629538f0aa80a707584c1ef462
lawliet0521LarryK <lawliet0521@gmail.com>
2020-12-10 16:20:35 +0900

    perf: migrated from travis ci to github actions ci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-docker login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_PASSWORD"
-
 echo $1
 
 if [[ $1 = "prod" ]]; then
-  TAG="$TRAVIS_TAG"
+  TAG="$GIT_TAG_NAME"
 fi
 
 docker build -t haechi/sdk-enclave:latest .
 docker push haechi/sdk-enclave:latest
 
-docker tag haechi/sdk-enclave:latest haechi/sdk-enclave:${TAG}
-docker push haechi/sdk-enclave:${TAG}
+docker tag haechi/sdk-enclave:latest haechi/sdk-enclave:TAG
+docker push haechi/sdk-enclave:TAG
