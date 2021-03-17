32ba12687c00baea0504fb125d4d194d4e34b57a
BoBeenLee <globaldev@naver.com>
2021-03-17 21:08:30 +0900

    fix: update v3 if testest wallet

--- packages/core/src/eth/wallets.ts
+++ packages/core/src/eth/wallets.ts
@@ -61,6 +61,17 @@ export class EthWallets extends Wallets<EthMasterWallet> {
     const walletData = await this.client.get<NoUndefinedField<MasterWalletDTO>>(
       `${this.baseUrl}/${id}`
     );
+    if (walletData.id === "b039b328c4880f64db6d1b35134a69cf") {
+      return new EthWallet(
+        this.client,
+        transformMasterWalletData({
+          ...walletData,
+          version: "v3",
+        }),
+        this.keychains,
+        this.blockchain
+      );
+    }
     return new EthWallet(
       this.client,
       transformMasterWalletData(walletData),
