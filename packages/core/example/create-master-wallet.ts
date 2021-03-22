d0939898efb5cd229ca31de81dce54c55671cfc2
BoBeenLee <globaldev@naver.com>
2021-03-22 16:08:41 +0900

    fix: change local env

--- packages/core/example/create-master-wallet.ts
+++ packages/core/example/create-master-wallet.ts
@@ -6,7 +6,7 @@ async function main() {
     accessToken: process.env.ACCESS_TOKEN,
     secret: process.env.SECRET,
     url: process.env.URL,
-    env: Env.Dev,
+    env: Env.Local,
   });
 
   const wallet = await sdk.btc.wallets.createMasterWallet(
