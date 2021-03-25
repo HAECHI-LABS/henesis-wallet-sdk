1580086f6d1646e7c24f2804b2d59f0a02b30a64
BoBeenLee <globaldev@naver.com>
2021-03-25 23:28:27 +0900

    feat: add notices mock

---
+++ packages/core/src/notices.ts
@@ -0,0 +1,25 @@
+import { Client } from "./httpClient";
+
+export interface Notice {
+  id: string;
+}
+
+export class Notices {
+  private readonly client: Client;
+
+  private readonly baseUrl = "/notices";
+
+  constructor(client: Client) {
+    this.client = client;
+  }
+
+  notices(): Promise<Notice[]> {
+    return Promise.resolve([
+      {
+        id: "1",
+      },
+    ]);
+  }
+
+  getNotice(request: { noticeId: string; seen: boolean }) {}
+}
