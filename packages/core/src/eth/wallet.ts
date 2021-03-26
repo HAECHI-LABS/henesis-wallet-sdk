2626ed7ff9fdebef291f589f7856972ad9ae8650
BoBeenLee <cultist_tp@naver.com>
2021-03-26 16:03:22 +0900

    SDK에서 web3-shh, web3-bzz 버전 업데이트 (#369)
    * chore: update web3 version
    
    * chore: update web3 version
    
    * fix: fixed type error
    
    * feat: add build test
    
    * fix: fixed any type
    
    * fix: remove unused any, script
    
    * fix: update exact version

--- packages/core/src/eth/wallet.ts
+++ packages/core/src/eth/wallet.ts
@@ -1,5 +1,5 @@
-import { Contract } from "web3-eth-contract/";
 import Web3 from "web3";
+import { Contract } from "web3-eth-contract";
 import { AbiItem } from "web3-utils";
 import BN from "bn.js";
 import {
