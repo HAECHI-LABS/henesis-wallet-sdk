46047d5f462efd72580f4492010d876b7c9cb355
OH YoungTaek <oyt311@hanmail.net>
2021-03-30 20:27:11 +0900

    refac: change dto structure

--- packages/enclave/src/v2/klay/dto/call-event.dto.ts
+++ packages/enclave/src/v2/klay/dto/call-event.dto.ts
@@ -1,25 +1,2 @@
-import { EventDTO } from "./event.dto";
-import { ApiProperty } from "@nestjs/swagger";
-
-export class CallEventDTO extends EventDTO {
-  /**
-   * 지갑 ID
-   * @example ETH
-   */
-
-  fromAddress: string;
-
-  /**
-   * 지갑 ID
-   * @example ETH
-   */
-
-  toAddress: string;
-
-  /**
-   * 지갑 ID
-   * @example ETH
-   */
-
-  data: string;
-}
+// todo: define
+export class CallEventDTO {}
