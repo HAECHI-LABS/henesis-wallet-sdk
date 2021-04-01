0723fff3c9c1917361e12221e0c10c7fd2e2a8b5
BoBeenLee <globaldev@naver.com>
2021-04-01 16:19:55 +0900

    fix: fixed walletv3 compare code

--- packages/core/src/utils/wallet.ts
+++ packages/core/src/utils/wallet.ts
@@ -1,7 +1,7 @@
 export const WALLET_VERSION_V3 = "v3";
 
 export const isLessThanWalletV3 = (version: string) => {
-  const verisonNumber = parseInt(this.getVersion().substr(1));
-  const v3VersionNumber = parseInt(WALLET_VERSION_V3);
-  return verisonNumber < v3VersionNumber;
+  const verisonNumber = parseInt(version);
+  const versionV3Number = parseInt(WALLET_VERSION_V3);
+  return verisonNumber < versionV3Number;
 };
