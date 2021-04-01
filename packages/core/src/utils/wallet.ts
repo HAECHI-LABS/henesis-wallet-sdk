0631da6118bb14724b5bda61262fdb09f9ccefbf
BoBeenLee <globaldev@naver.com>
2021-04-01 16:32:48 +0900

    fix: fixed less than walletv3

--- packages/core/src/utils/wallet.ts
+++ packages/core/src/utils/wallet.ts
@@ -1,7 +1,7 @@
 export const WALLET_VERSION_V3 = "v3";
 
 export const isLessThanWalletV3 = (version: string) => {
-  const verisonNumber = parseInt(version);
-  const versionV3Number = parseInt(WALLET_VERSION_V3);
-  return verisonNumber < versionV3Number;
+  const verisonNumber = parseInt(version.substr(1));
+  const v3VersionNumber = parseInt(WALLET_VERSION_V3.substr(1));
+  return verisonNumber < v3VersionNumber;
 };
