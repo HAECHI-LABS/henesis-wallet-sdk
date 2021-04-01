c5d2853e43d184d6569a1fda5bef8ca262533cd9
BoBeenLee <globaldev@naver.com>
2021-04-01 16:33:15 +0900

    fix: rename v3 number

--- packages/core/src/utils/wallet.ts
+++ packages/core/src/utils/wallet.ts
@@ -2,6 +2,6 @@ export const WALLET_VERSION_V3 = "v3";
 
 export const isLessThanWalletV3 = (version: string) => {
   const verisonNumber = parseInt(version.substr(1));
-  const v3VersionNumber = parseInt(WALLET_VERSION_V3.substr(1));
-  return verisonNumber < v3VersionNumber;
+  const versionV3Number = parseInt(WALLET_VERSION_V3.substr(1));
+  return verisonNumber < versionV3Number;
 };
