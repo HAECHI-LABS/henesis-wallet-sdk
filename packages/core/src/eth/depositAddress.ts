1d9fe6337959167150a30b810683a5eba9aa00d7
BoBeenLee <globaldev@naver.com>
2021-04-05 18:51:22 +0900

    fix: fixed circular dep error

--- packages/core/src/eth/depositAddress.ts
+++ packages/core/src/eth/depositAddress.ts
@@ -9,7 +9,11 @@ import {
   ChangeWalletNameRequest,
 } from "../__generate__/eth";
 import _ from "lodash";
-import { EthLikeWallet, EthWalletData, EthMasterWalletData } from "./wallet";
+import {
+  EthLikeWallet,
+  EthWalletData,
+  EthMasterWalletData,
+} from "./abstractWallet";
 import { transformWalletStatus } from "../wallet";
 
 export const transformDepositAddressData = (
