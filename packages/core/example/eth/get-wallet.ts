31b321e26a8c2a83dd2db9759772b757afef8501
BoBeenLee <globaldev@naver.com>
2021-04-20 12:13:03 +0900

    Revert "fix: remove eth v3"
    This reverts commit da3e96c37ce8ddff7163a73d723937f9268d760a.

---
+++ packages/core/example/eth/get-wallet.ts
@@ -0,0 +1,19 @@
+/// <reference path="../../src/typings/index.d.ts" />
+import { SDK, Env } from "../../src";
+import "dotenv/config";
+
+async function main() {
+  const sdk = new SDK({
+    accessToken: process.env.ACCESS_TOKEN,
+    secret: process.env.SECRET,
+    url: process.env.URL,
+    env: Env.Local,
+  });
+
+  const wallet = await sdk.eth.wallets.getWallet(
+    "3be5351bd52626108326f9ec44b7b633"
+  );
+  console.log(wallet.getData());
+}
+
+main();
