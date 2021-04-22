533391b155937ca89e635ff44c5ee455ae2ea54b
BoBeenLee <globaldev@naver.com>
2021-04-22 13:35:30 +0900

    fix: add get notices example

---
+++ example/get-notices.ts
@@ -0,0 +1,17 @@
+/// <reference path="../src/typings/index.d.ts" />
+import { SDK, Env } from "../src";
+import "dotenv/config";
+
+async function main() {
+  const sdk = new SDK({
+    accessToken: process.env.ACCESS_TOKEN,
+    secret: process.env.SECRET,
+    url: process.env.URL,
+    env: Env.Local,
+  });
+
+  const response = await sdk.notices.getNotices();
+  console.log(response);
+}
+
+main();
