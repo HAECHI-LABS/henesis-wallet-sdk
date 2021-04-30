f52d326d89dea9fec195b2b3774b3ac6be7e6fc0
BoBeenLee <globaldev@naver.com>
2021-04-30 14:59:19 +0900

    feat: add example get flush histories

---
+++ example/eth/get-flush-histories.ts
@@ -0,0 +1,21 @@
+/// <reference path="../../src/typings/index.d.ts" />
+import { SDK, Env } from "../../src";
+import "dotenv/config";
+import { Pagination } from "../../src/types";
+
+async function main() {
+  const sdk = new SDK({
+    accessToken: process.env.ACCESS_TOKEN,
+    secret: process.env.SECRET,
+    url: process.env.URL,
+    env: Env.Local,
+  });
+
+  const wallet = await sdk.eth.wallets.getWallet(
+    "3be5351bd52626108326f9ec44b7b633"
+  );
+  const response = await wallet.getFlushTransactions({});
+  console.log(response);
+}
+
+main();
