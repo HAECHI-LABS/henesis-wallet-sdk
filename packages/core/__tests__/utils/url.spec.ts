40c4f57345ab8f60adf35f21185d5f91621b3aea
BoBeenLee <globaldev@naver.com>
2021-05-01 18:38:10 +0900

    fix: fixed makeQueryString bug

--- packages/core/__tests__/utils/url.spec.ts
+++ packages/core/__tests__/utils/url.spec.ts
@@ -5,6 +5,8 @@ describe('url.spec.ts', () => {
   describe('#makeQueryString', () => {
     it('should make makeQueryString', () => {
       expect(makeQueryString({ a: 1, b: 2 })).toEqual('a=1&b=2');
+      expect(makeQueryString({ a: 0, b: 2 })).toEqual('a=0&b=2');
+      expect(makeQueryString({ a: null, b: undefined })).toEqual('a=null');
     });
     it('should make makeQueryString empty', () => {
       expect(makeQueryString({})).toEqual('');
