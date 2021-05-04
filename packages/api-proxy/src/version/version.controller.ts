99f49cc0eee59abac91b52b332447b818ac77e40
OH YoungTaek <oyt311@hanmail.net>
2021-05-04 16:57:09 +0900

    feat: add version

--- packages/api-proxy/src/version/version.controller.ts
+++ packages/api-proxy/src/version/version.controller.ts
@@ -1,4 +1,9 @@
 import { Controller } from "@nestjs/common";
+import * as PACKAGE from "../../package.json";
 
 @Controller("/version")
-export default class VersionController {}
+export default class VersionController {
+  getVersion() {
+    return PACKAGE.version;
+  }
+}
