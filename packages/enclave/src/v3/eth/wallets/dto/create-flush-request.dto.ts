490aca694708a990f97040da94182a494d57c7f9
OH YoungTaek <oyt311@hanmail.net>
2021-05-04 14:05:36 +0900

    fix: remove ticker from flush request(v3)

--- packages/enclave/src/v3/eth/wallets/dto/create-flush-request.dto.ts
+++ packages/enclave/src/v3/eth/wallets/dto/create-flush-request.dto.ts
@@ -14,11 +14,6 @@ class Target {
 }
 
 export class CreateFlushRequestDTO {
-  /**
-   * 암호화폐의 기호 (ticker)
-   * @example ETH
-   */
-  ticker: string;
   targets: Target[];
   /**
    * 트랜잭션에 사용할 gas price (단위: wei) null일 경우, Henesis가 자동으로 5분 안에 채굴될 수 있는 값으로 설정합니다.
