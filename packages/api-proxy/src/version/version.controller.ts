8514c26d0c3ae8a7f5b088a55c0d1b9e8401b39c
OH YoungTaek <oyt311@hanmail.net>
2021-05-08 11:08:26 +0900

    fix: add DTO for /api/version controller

--- packages/api-proxy/src/version/version.controller.ts
+++ packages/api-proxy/src/version/version.controller.ts
@@ -1,11 +1,18 @@
 import { Controller, Get } from "@nestjs/common";
+import { VersionDTO } from "./dto/version.dto";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 
 const PACKAGE = require("../../package.json");
 
 @Controller("/version")
+@ApiTags("version")
 export default class VersionController {
   @Get("")
-  getVersion() {
-    return PACKAGE.version;
+  @ApiOperation({
+    summary: "API Proxy 버전",
+    description: "현재 API Proxy의 버전을 반환합니다",
+  })
+  getVersion(): VersionDTO {
+    return { version: PACKAGE.version };
   }
 }
