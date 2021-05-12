f8d4af3ca2e5ed98f3fa6af894334a35ec737eab
HaruHuey <kkh555999@naver.com>
2021-05-12 19:42:14 +0900

    feat: add error model property description

--- packages/api-proxy/src/extra-model.dto.ts
+++ packages/api-proxy/src/extra-model.dto.ts
@@ -1,52 +1,58 @@
 import { ApiModelProperty } from '@nestjs/swagger/dist/decorators/api-model-property.decorator';
 
-export const INVALID_ACCESS_TOKEN_EXCEPTION_EXAMPLE: InvalidAccessTokenException = {
+export const EXAMPLE_INVALID_ACCESS_TOKEN_EXCEPTION: InvalidAccessTokenException = {
   message: "expired or invalid access token and signature",
   code: 4001
 }
 
-export const ACCESS_TOKEN_NOT_PROVIDED_EXCEPTION_EXAMPLE: AccessTokenNotProvidedException = {
+export const EXAMPLE_ACCESS_TOKEN_NOT_PROVIDED_EXCEPTION: AccessTokenNotProvidedException = {
   message: "access token not provided",
   code: 4001
 }
 
-export const INVALID_ACCESS_IP_EXCEPTION_EXAMPLE: InvalidAccessIpException = {
+export const EXAMPLE_INVALID_ACCESS_IP_EXCEPTION: InvalidAccessIpException = {
   message: "ip '106.101.128.133' is not allowed",
   code: 4008
 }
 
 export class InvalidAccessTokenException {
   @ApiModelProperty({
-    example: INVALID_ACCESS_TOKEN_EXCEPTION_EXAMPLE.message
+    description: "API 에러 메시지",
+    example: EXAMPLE_INVALID_ACCESS_TOKEN_EXCEPTION.message
   })
   message: string;
 
   @ApiModelProperty({
-    example: INVALID_ACCESS_TOKEN_EXCEPTION_EXAMPLE.code
+    description: "헤네시스 API 에러 코드",
+    example: EXAMPLE_INVALID_ACCESS_TOKEN_EXCEPTION.code
   })
   code: number;
 }
 
 export class AccessTokenNotProvidedException {
   @ApiModelProperty({
-    example: ACCESS_TOKEN_NOT_PROVIDED_EXCEPTION_EXAMPLE.message
+    description: "API 에러 메시지",
+    example: EXAMPLE_ACCESS_TOKEN_NOT_PROVIDED_EXCEPTION.message
   })
   message: string;
 
   @ApiModelProperty({
-    example: ACCESS_TOKEN_NOT_PROVIDED_EXCEPTION_EXAMPLE.code
+    description: "헤네시스 API 에러 코드",
+    example: EXAMPLE_ACCESS_TOKEN_NOT_PROVIDED_EXCEPTION.code
   })
   code: number;
 }
 
 export class InvalidAccessIpException {
   @ApiModelProperty({
-    example: INVALID_ACCESS_IP_EXCEPTION_EXAMPLE.message
+    description: "API 에러 메시지",
+    example: EXAMPLE_INVALID_ACCESS_IP_EXCEPTION.message
   })
   message: string;
 
   @ApiModelProperty({
-    example: INVALID_ACCESS_IP_EXCEPTION_EXAMPLE.code
+    description: "헤네시스 API 에러 코드",
+    example: EXAMPLE_INVALID_ACCESS_IP_EXCEPTION.code
   })
   code: number;
 }
