69d59e00c507e1c8334984ed01d9594845be4908
Junhui Kim <junhuikim.dev@gmail.com>
2021-06-02 00:11:49 +0900

    fix: fix filecoin SDK interfaces

--- packages/core/src/fil/feeWallets.ts
+++ packages/core/src/fil/feeWallets.ts
@@ -1,12 +1,18 @@
 import {
+  BalanceDTO,
   FeeHistoryDTO,
   FeeWalletBalanceDTO,
   FeeWalletDTO,
+  ProposalWalletBalanceDTO,
 } from "../__generate__/fil";
-import { FilHenesisKey } from "./henesisKeys";
 import { Key, Pagination } from "../types";
 import { Client } from "../httpClient";
 import { FilTransaction } from "./abstractWallet";
+import BN from "bn.js";
+
+export interface FilHenesisKey extends Key {
+  keyId: string;
+}
 
 export interface FilFeeWallet
   extends Omit<FeeWalletDTO, "defaultWallet" | "proposalWallets"> {
@@ -14,7 +20,22 @@ export interface FilFeeWallet
   proposalWallets: Key[];
 }
 
-export type FilFeeWalletBalance = FeeWalletBalanceDTO;
+export interface FilBalance
+  extends Omit<BalanceDTO, "balance" | "spendableBalance"> {
+  balance: BN;
+  spendableBalance: BN;
+}
+
+export interface ProposalWalletBalance
+  extends Omit<ProposalWalletBalanceDTO, "balance"> {
+  balance: FilBalance;
+}
+
+export interface FilFeeWalletBalance
+  extends Omit<FeeWalletBalanceDTO, "defaultWallet" | "proposalWallets"> {
+  defaultWallet: FilBalance;
+  proposalWallets: ProposalWalletBalance[];
+}
 
 export interface FilFeeHistory extends Omit<FeeHistoryDTO, "transaction"> {
   transaction: FilTransaction;
@@ -39,6 +60,6 @@ export class FilFeeWallets {
 
   // TODO: implement me
   async getFeeHistories(): Promise<Pagination<FilFeeHistory>> {
-    return null;
+    throw new Error("this feature is not supported yet");
   }
 }
