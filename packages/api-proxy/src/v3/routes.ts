24dd495dcb2d6c85fbc82608c61f5bdde7da922e
borre <borre@haechi.io>
2021-06-11 19:05:40 +0900

    chore: remove an empty file

--- packages/api-proxy/src/v3/routes.ts
+++
@@ -1,6 +0,0 @@
-// import { Routes } from "nest-router";
-// import { EthModule } from "./eth/eth.module";
-//
-// export const routes: Routes = [
-//   { path: "ethereum", module: EthModule },
-// ] as Routes;
