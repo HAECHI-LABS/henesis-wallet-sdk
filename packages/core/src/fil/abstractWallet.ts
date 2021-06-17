25db26ae24ad0bbca49cd98f879510c83a3cce83
Junhui Kim <junhuikim.dev@gmail.com>
2021-06-17 15:07:31 +0900

    fix: add gasPremium to transfer and flush

--- packages/core/src/fil/abstractWallet.ts
+++ packages/core/src/fil/abstractWallet.ts
@@ -9,6 +9,10 @@ import {
 import { FilKeychains } from "./keychains";
 import BN from "bn.js";
 
+export const FilGasLimit = {
+  PROPOSE: new BN(4800000),
+};
+
 export interface FilTransaction
   extends Omit<TransactionDTO, "nonce" | "amount" | "feeAmount"> {
   nonce: BN;
