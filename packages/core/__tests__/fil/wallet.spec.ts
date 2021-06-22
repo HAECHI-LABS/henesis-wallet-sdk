4f95ad447c5675d7bc8ba575cb8b2b7477bc648b
Junhui Kim <junhuikim.dev@gmail.com>
2021-06-22 17:28:28 +0900

    fix: fix creating deposit address bug in filecoin

--- packages/core/__tests__/fil/wallet.spec.ts
+++ packages/core/__tests__/fil/wallet.spec.ts
@@ -24,4 +24,28 @@ describe('FilWallet', () => {
       console.log(flush);
     })
   })
+
+  describe.skip('#createDepositAddress()', () => {
+    it('should create deposit address', async () => {
+      const client = new HttpClient({
+        accessToken: 'eyJhbGciOiJIUzUxMiJ9.eyJlbWFpbCI6InByb2R1Y3RAaGFlY2hpLmlvIiwiaWQiOiI0OTgwM2ZiZjBhMmEyYzNhYjNhMGFiYThiOThkYmIyYSIsInR5cGUiOiJMT05HIiwibG9uZ1R5cGUiOnRydWUsImlzcyI6ImhlbmVzaXMtd2FsbGV0LWRldiIsImlhdCI6MTYyMDA0MTYzMywiZXhwIjoxNjUxNTc3NjMzfQ.iP_uFvsQUymdBJFjAmH8VyVnWdpfoGTazGXkllob7b2qjcEKK13pymn3QxNkMAt5IXN7uVQ35EJsRgf7MEMUbQ',
+        secret: 'f2sGpgMbhBavVihRyrFp4trhHP7RdOplYDhxY3pGn+c=',
+        url: 'https://dev.wallet.henesis.io/api/v2/fil',
+        env: Env.Dev,
+      }) as any;
+      const wallets = new FilWallets(
+        client,
+        new FilKeychains(Env.Dev),
+        Env.Dev,
+        BlockchainType.FILECOIN,
+        new FilFeeWallets(client)
+      );
+      const wallet = await wallets.getWallet('d6c86b80ce2e7029a8efb99c4a13773c');
+      const depositAddressData = await wallet.createDepositAddress(`deposit-address-test-${Math.random()}`, 'password');
+      console.log(depositAddressData);
+
+      const depositAddress = await wallet.getDepositAddress(depositAddressData.getId());
+      expect(depositAddress.getAddress()).toEqual(depositAddressData.getAddress());
+    })
+  });
 })
--- packages/core/__tests__/fil/wallets.spec.ts
+++ packages/core/__tests__/fil/wallets.spec.ts
@@ -25,4 +25,26 @@ describe('FilWallets', () => {
       expect(wallets.verifyAddress('f1lbltetimfvmo252wuq7i3244zvrkr5lvd5cof5a')).toEqual(true);
     })
   })
+
+  describe.skip('#createWallet()', () => {
+    it('should create wallet', async () => {
+      const client = new HttpClient({
+        accessToken: 'accessToken',
+        secret: 'secret',
+        url: 'http://localhost:8080/api/v2/fil',
+        env: Env.Local,
+      }) as any;
+      const wallets = new FilWallets(
+        client,
+        new FilKeychains(Env.Local),
+        Env.Local,
+        BlockchainType.FILECOIN,
+        new FilFeeWallets(client)
+      );
+
+      const recoveryKit = await wallets.createRecoveryKit('test-wallet', 'password');
+      const wallet = await wallets.createWalletWithKit(recoveryKit);
+      console.log(wallet);
+    })
+  })
 })
