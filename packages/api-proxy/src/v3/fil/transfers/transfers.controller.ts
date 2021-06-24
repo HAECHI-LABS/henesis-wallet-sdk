6cf1bc38633708b677c74fcaf2e2204e6f344253
Junhui Kim <junhuikim.dev@gmail.com>
2021-06-25 01:26:23 +0900

    fix: fix api proxy not running bug

--- packages/api-proxy/src/v3/fil/transfers/transfers.controller.ts
+++ packages/api-proxy/src/v3/fil/transfers/transfers.controller.ts
@@ -9,18 +9,35 @@ import {
   ReadMeExtension,
 } from "../../../decorators";
 import express from "express";
-import { TransferType } from "@haechi-labs/henesis-wallet-core/lib/__generate__/eth";
 import {
   EXAMPLE_FILECOIN_PAGINATION_TRANSFER_DTO,
   PaginationDTO,
 } from "../dto/pagination.dto";
 import { TransferDTO } from "../dto/transfer.dto";
 import { TransfersService } from "./transfers.service";
-import { TransferStatus } from "@haechi-labs/henesis-wallet-core/lib/__generate__/fil";
+import {
+  TransferStatus,
+  TransferType,
+} from "@haechi-labs/henesis-wallet-core/lib/__generate__/fil";
 import {
   EXAMPLE_INVALID_STATUS_EXCEPTION_DTO,
   InvalidStatusException,
 } from "../dto/exceptions.dto";
+import {
+  TRANSACTION_HASH_OPTIONAL,
+  TRANSACTION_ID_OPTIONAL,
+  WALLET_ID_OPTIONAL,
+} from "../wallets/dto/params.dto";
+import {
+  TRANSFER_STATUS_OPTIONAL,
+  TRANSFER_TYPE_OPTIONAL,
+} from "./dto/params.dto";
+import {
+  PAGE_OPTIONAL,
+  SIZE_OPTIONAL,
+  UPDATED_AT_GTE_OPTIONAL,
+  UPDATED_AT_LE_OPTIONAL,
+} from "../dto/params";
 
 @Controller("transfers")
 @ApiTags("transfers")
@@ -31,7 +48,17 @@ export class TransfersController {
   constructor(private readonly transfersService: TransfersService) {}
 
   @Get("/")
-  @Queries()
+  @Queries(
+    WALLET_ID_OPTIONAL,
+    TRANSACTION_ID_OPTIONAL,
+    TRANSACTION_HASH_OPTIONAL,
+    TRANSFER_STATUS_OPTIONAL,
+    TRANSFER_TYPE_OPTIONAL,
+    UPDATED_AT_GTE_OPTIONAL,
+    UPDATED_AT_LE_OPTIONAL,
+    SIZE_OPTIONAL,
+    PAGE_OPTIONAL
+  )
   @ApiPaginationResponse(TransferDTO, EXAMPLE_FILECOIN_PAGINATION_TRANSFER_DTO)
   @ApiBadRequestResponse({
     description: "올바르지 않은 입출금 상태(status)로 요청하면 발생합니다.",
