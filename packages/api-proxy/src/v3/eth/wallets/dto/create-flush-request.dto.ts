b483db1be31be71fcb9f79536576beb312bdd9dc
JhChoy <jchoyeclipse@gmail.com>
2021-06-29 15:46:21 +0900

    fix: fix v3 eth input type bug and fix some docs annotation

--- packages/api-proxy/src/v3/eth/wallets/dto/create-flush-request.dto.ts
+++ packages/api-proxy/src/v3/eth/wallets/dto/create-flush-request.dto.ts
@@ -1,38 +1,58 @@
-import { ApiPropertyOptional } from "@nestjs/swagger";
-import { ApiModelPropertyOptional } from "@nestjs/swagger/dist/decorators/api-model-property.decorator";
+import {
+  ApiModelProperty,
+  ApiModelPropertyOptional,
+} from "@nestjs/swagger/dist/decorators/api-model-property.decorator";
+
+export const EXAMPLE_FLUSH_TARGET_DTO: Target = {
+  coinId: "11",
+  depositAddressId: "c2bd6506cb56a6baaff32653ac77ef49",
+};
+
+export const EXAMPLE_CREATE_FLUSH_REQUEST_DTO: CreateFlushRequestDTO = {
+  targets: [EXAMPLE_FLUSH_TARGET_DTO],
+  gasPrice: "8000000000",
+  gasLimit: "10000000000",
+  metadata: "metadata",
+};
 
 class Target {
-  /**
-   * Henesis에서 부여한 Coin의 ID
-   * @example 11
-   */
+  @ApiModelProperty({
+    description: "Henesis에서 부여한 Coin의 ID",
+    example: EXAMPLE_FLUSH_TARGET_DTO.coinId,
+  })
   coinId: string;
-  /**
-   * 입금 주소 ID
-   * @example c2bd6506cb56a6baaff32653ac77ef49
-   */
+
+  @ApiModelProperty({
+    description: "입금 주소 ID",
+    example: EXAMPLE_FLUSH_TARGET_DTO.coinId,
+  })
   depositAddressId: string;
 }
 
 export class CreateFlushRequestDTO {
+  @ApiModelProperty({
+    description: "집금할 대상의 목록",
+    example: EXAMPLE_CREATE_FLUSH_REQUEST_DTO.targets,
+  })
   targets: Target[];
-  /**
-   * 트랜잭션에 사용할 gas price (단위: wei) null일 경우, Henesis가 자동으로 5분 안에 채굴될 수 있는 값으로 설정합니다.
-   * @example: 8000000000
-   */
-  @ApiPropertyOptional()
-  gasPrice: number;
-  /**
-   * 트랜잭션에 사용할 gas limit (단위: wei) null일 경우, Henesis가 자동으로 설정합니다.
-   * @example: 10000000000
-   */
-  @ApiPropertyOptional()
-  gasLimit: number;
+
+  @ApiModelPropertyOptional({
+    description:
+      "트랜잭션에 사용할 gas price (단위: wei) null일 경우, Henesis가 자동으로 5분 안에 채굴될 수 있는 값으로 설정합니다.",
+    example: EXAMPLE_CREATE_FLUSH_REQUEST_DTO.gasPrice,
+  })
+  gasPrice: string;
+
+  @ApiModelPropertyOptional({
+    description:
+      "트랜잭션에 사용할 gas limit null일 경우, Henesis가 자동으로 설정합니다.",
+    example: EXAMPLE_CREATE_FLUSH_REQUEST_DTO.gasLimit,
+  })
+  gasLimit: string;
 
   @ApiModelPropertyOptional({
     description: "기타 정보 기록용 메타 데이터 (255자 제한)",
     example: "metadata",
-    default: "metadata",
   })
   metadata?: string;
 }
