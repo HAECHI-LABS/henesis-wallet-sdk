8b59cdd45be1582b367fcd96178a932aa8b4d2c3
Junhui Kim <junhuikim.dev@gmail.com>
2021-06-29 17:00:05 +0900

    fix: fix balance and request dto in v3 ethereum api proxy

--- packages/api-proxy/src/v3/eth/wallets/dto/create-flush-request.dto.ts
+++ packages/api-proxy/src/v3/eth/wallets/dto/create-flush-request.dto.ts
@@ -3,28 +3,29 @@ import {
   ApiModelPropertyOptional,
 } from "@nestjs/swagger/dist/decorators/api-model-property.decorator";
 
-export const EXAMPLE_FLUSH_TARGET_DTO: Target = {
-  coinId: "11",
+export const EXAMPLE_ETHEREUM_FLUSH_TARGET_DTO: Target = {
+  coinId: 11,
   depositAddressId: "c2bd6506cb56a6baaff32653ac77ef49",
 };
 
-export const EXAMPLE_CREATE_FLUSH_REQUEST_DTO: CreateFlushRequestDTO = {
-  targets: [EXAMPLE_FLUSH_TARGET_DTO],
-  gasPrice: "8000000000",
-  gasLimit: "10000000000",
-  metadata: "metadata",
-};
+export const EXAMPLE_ETHEREUM_CREATE_FLUSH_REQUEST_DTO: CreateFlushRequestDTO =
+  {
+    targets: [EXAMPLE_ETHEREUM_FLUSH_TARGET_DTO],
+    gasPrice: "8000000000",
+    gasLimit: "10000000000",
+    metadata: "metadata",
+  };
 
 class Target {
   @ApiModelProperty({
     description: "Henesis에서 부여한 Coin의 ID",
-    example: EXAMPLE_FLUSH_TARGET_DTO.coinId,
+    example: EXAMPLE_ETHEREUM_FLUSH_TARGET_DTO.coinId,
   })
-  coinId: string;
+  coinId: number;
 
   @ApiModelProperty({
     description: "입금 주소 ID",
-    example: EXAMPLE_FLUSH_TARGET_DTO.coinId,
+    example: EXAMPLE_ETHEREUM_FLUSH_TARGET_DTO.depositAddressId,
   })
   depositAddressId: string;
 }
@@ -32,21 +33,21 @@ class Target {
 export class CreateFlushRequestDTO {
   @ApiModelProperty({
     description: "집금할 대상의 목록",
-    example: EXAMPLE_CREATE_FLUSH_REQUEST_DTO.targets,
+    example: EXAMPLE_ETHEREUM_CREATE_FLUSH_REQUEST_DTO.targets,
   })
   targets: Target[];
 
   @ApiModelPropertyOptional({
     description:
-      "트랜잭션에 사용할 gas price (단위: wei) null일 경우, Henesis가 자동으로 5분 안에 채굴될 수 있는 값으로 설정합니다.",
-    example: EXAMPLE_CREATE_FLUSH_REQUEST_DTO.gasPrice,
+      "트랜잭션에 사용할 gas price 입니다. (단위: wei) null일 경우, Henesis가 자동으로 5분 안에 채굴될 수 있는 값으로 설정합니다.",
+    example: EXAMPLE_ETHEREUM_CREATE_FLUSH_REQUEST_DTO.gasPrice,
   })
   gasPrice: string;
 
   @ApiModelPropertyOptional({
     description:
-      "트랜잭션에 사용할 gas limit null일 경우, Henesis가 자동으로 설정합니다.",
-    example: EXAMPLE_CREATE_FLUSH_REQUEST_DTO.gasLimit,
+      "트랜잭션에 사용할 gas limit 입니다. null일 경우, Henesis가 자동으로 설정합니다.",
+    example: EXAMPLE_ETHEREUM_CREATE_FLUSH_REQUEST_DTO.gasLimit,
   })
   gasLimit: string;
 
