8e81f2b1d3adc31076824f958dc7d70967300d61
JhChoy <jchoyeclipse@gmail.com>
2021-07-29 14:53:38 +0900

    fix: add gas limit option at eth v3

--- packages/api-proxy/src/v3/eth/wallets/dto/send-coin-request.dto.ts
+++ packages/api-proxy/src/v3/eth/wallets/dto/send-coin-request.dto.ts
@@ -9,6 +9,7 @@ export const EXAMPLE_ETHEREUM_SEND_COIN_REQUEST_DTO: SendCoinRequestDTO = {
   amount: "100000000",
   passphrase: "passphrase",
   gasPrice: "8000000000",
+  gasLimit: "500000",
   metadata: "metadata",
 };
 
@@ -42,7 +43,14 @@ export class SendCoinRequestDTO {
       "트랜잭션에 사용할 gas price (단위: wei) null일 경우, Henesis가 자동으로 5분 안에 채굴될 수 있는 값으로 설정합니다.",
     example: EXAMPLE_ETHEREUM_SEND_COIN_REQUEST_DTO.gasPrice,
   })
-  gasPrice: string;
+  gasPrice?: string;
+
+  @ApiModelPropertyOptional({
+    description:
+      "트랜잭션에 사용할 gas limit. null일 경우, Henesis가 자동으로 설정합니다.",
+    example: EXAMPLE_ETHEREUM_SEND_COIN_REQUEST_DTO.gasLimit,
+  })
+  gasLimit?: string;
 
   @ApiModelPropertyOptional({
     description: "기타 정보 기록용 메타 데이터 (255자 제한)",
