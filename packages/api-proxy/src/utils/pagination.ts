06d38b6009af5523f6cad6ee5fa377ac4d6cd8a3
JhChoy <jchoyeclipse@gmail.com>
2021-08-03 12:47:18 +0900

    chore: update package-lock.json

--- packages/api-proxy/src/utils/pagination.ts
+++ packages/api-proxy/src/utils/pagination.ts
@@ -6,5 +6,7 @@ export function changeUrlHost(url: string, request: express.Request) {
     return url;
   }
   const index = url.indexOf("?");
-  return `${request.protocol}://${request.headers.host}${request.path}${toCamelCase(url.slice(index))}`;
+  return `${request.protocol}://${request.headers.host}${
+    request.path
+  }${toCamelCase(url.slice(index))}`;
 }
