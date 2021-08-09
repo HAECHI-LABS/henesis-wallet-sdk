6adeb1b3162028fee24156775e8b9a1622f68a81
Park Juhyung(Borre) <borre@haechi.io>
2021-08-09 16:49:37 +0900

    feat: add dummy LTC implementation (#531)

---
+++ packages/core/src/apis/ltc/wallet.ts
@@ -0,0 +1,45 @@
+import _ from "lodash";
+import { AxiosInstance } from "axios";
+import {
+  WalletControllerApiFactory,
+  CreateDepositAddressRequest,
+} from "../../__generate__/ltc";
+import { Client } from "../../httpClient";
+
+const walletControllerApi = (client: Client) => {
+  const apiClient: AxiosInstance = _.get(client, ["apiClient"]);
+  // eslint-disable-next-line new-cap
+  return WalletControllerApiFactory(undefined, "", apiClient);
+};
+
+export const createDepositAddressApi = async ({
+  client,
+  walletId,
+  request,
+}: {
+  client: Client;
+  walletId: string;
+  request: CreateDepositAddressRequest;
+}) => {
+  const response = await walletControllerApi(client).createDepositAddress(
+    walletId,
+    request
+  );
+  return response.data;
+};
+
+export const getDepositAddressApi = async ({
+  client,
+  walletId,
+  depositAddressId,
+}: {
+  client: Client;
+  walletId: string;
+  depositAddressId: string;
+}) => {
+  const response = await walletControllerApi(client).getDepositAddress(
+    walletId,
+    depositAddressId
+  );
+  return response.data;
+};
