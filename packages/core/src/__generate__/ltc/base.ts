164ee30e2c8ddfb93f5b0e25aae00cd683c03784
Park Juhyung(Borre) <borre@haechi.io>
2021-08-24 19:02:02 +0900

    feat: give legacy p2sh address together when transfer LTC (#549)

--- packages/core/src/__generate__/ltc/base.ts
+++ packages/core/src/__generate__/ltc/base.ts
@@ -17,7 +17,7 @@ import { Configuration } from "./configuration";
 // @ts-ignore
 import globalAxios, { AxiosPromise, AxiosInstance } from 'axios';
 
-export const BASE_PATH = "http://127.0.0.1:8080".replace(/\/+$/, "");
+export const BASE_PATH = "https://dev.wallet.henesis.io:80".replace(/\/+$/, "");
 
 /**
  *
