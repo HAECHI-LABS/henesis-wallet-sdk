0f51872a726811105ac7fe48ad81049de66d2a12
AgwaB <leesd556@gmail.com>
2021-09-03 18:39:35 +0900

    feat: implements bch tx build logic

--- packages/core/src/bch/utils.ts
+++ packages/core/src/bch/utils.ts
@@ -6,6 +6,12 @@ import {
 import { Transfer, TransferInternal } from "./transfers";
 import { BchTransaction, BchTransactionOutput } from "./wallet";
 import { BNConverter } from "../utils/common";
+import { Buffer } from "buffer";
+import CashAddress from "./cashAddress";
+import { add } from "../../lib/eth/eth-core-lib/nat";
+const cashAddress = new CashAddress();
+const typeforce = require("typeforce");
+const bip66 = require("bip66");
 
 export const convertTransactionDTO = (
   transaction: BtcTransactionDTO
@@ -57,3 +63,38 @@ export const convertTransferInternalDTO = (
     confirmation: BNConverter.hexStringToBN(transfer.confirmation),
   };
 };
+
+export const toLegacyAddress = (address: string) => {
+  return cashAddress.toLegacyAddress(address);
+};
+
+// ref: https://github.com/bitcoinjs/bitcoinjs-lib/blob/master/src/script_signature.js#L35
+// this is to use bitcoinjs-lib for bch
+// bitcoin cash use sigHash 'FORKID' and bitcoinjs-lib cannot translate 'FORKID'.
+export const encodeScriptSignature = (signature: Buffer, hashType: number) => {
+  typeforce(
+    {
+      signature: typeforce.BufferN(64),
+      hashType: typeforce.UInt8,
+    },
+    { signature, hashType }
+  );
+  // const hashTypeMod = hashType & ~0x80;
+  // if (hashTypeMod <= 0 || hashTypeMod >= 4)
+  //   throw new Error('Invalid hashType ' + hashType);
+  const hashTypeBuffer = Buffer.allocUnsafe(1);
+  hashTypeBuffer.writeUInt8(hashType, 0);
+  const r = toDER(signature.slice(0, 32));
+  const s = toDER(signature.slice(32, 64));
+  return Buffer.concat([bip66.encode(r, s), hashTypeBuffer]);
+};
+
+const ZERO = Buffer.alloc(1, 0);
+function toDER(x) {
+  let i = 0;
+  while (x[i] === 0) ++i;
+  if (i === x.length) return ZERO;
+  x = x.slice(i);
+  if (x[0] & 0x80) return Buffer.concat([ZERO, x], 1 + x.length);
+  return x;
+}
