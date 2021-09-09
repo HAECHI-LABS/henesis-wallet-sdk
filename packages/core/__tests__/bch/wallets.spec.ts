1bbfbdbb209d096b8a08c2013ab241ea53e93afa
BoBeenLee <globaldev@naver.com>
2021-09-09 12:20:50 +0900

    feat: add old address test

--- packages/core/__tests__/bch/wallets.spec.ts
+++ packages/core/__tests__/bch/wallets.spec.ts
@@ -19,4 +19,20 @@ describe('BchWallets', () => {
       expect(wallets.verifyAddress('bchtest:pp7zq5cy7tz6xsteesfydnlrna47zxm4xq')).toEqual(true);
     });
   });
+
+  it('should return false when old address is not from dev', async () => {
+    const wallets = new BchWallets(
+      Env.Dev,
+      new HttpClient({
+        accessToken: 'accessToken',
+        secret: 'secret',
+        url: 'http://localhost:8080/api/v2/bch',
+        env: Env.Dev,
+      }) as any,
+      new BchKeyChains(Env.Dev),
+    );
+
+    expect(wallets.verifyAddress('2MvNDajCeC9qCqezstGXU436qmH4ajr4N2v')).toEqual(true);
+  });
+});
 });
